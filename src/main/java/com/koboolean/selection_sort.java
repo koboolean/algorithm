5b3877e6fc6f618b65ebf4f589b26fc2cbd76d22
조현준 <45157196+koboolean@users.noreply.github.com>
2024-08-22 16:36:31 +0900

    선택정렬 알고리즘 관련사항 반영

---
+++ src/main/java/com/koboolean/selection_sort.java
@@ -0,0 +1,51 @@
+package com.koboolean;
+
+public class selection_sort {
+
+    public static void main(String[] args) {
+        System.out.println("선택정렬");
+        int[] result = selectionSort(new int[]{1,6,4,3,7,8,12,7,3,4,6,18,4,2,6,24});
+
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    /**
+     * 데이터 sort를 진행한다.
+     * @param data
+     * @return
+     */
+    public static int[] selectionSort(int[] data) {
+        // 배열의 마지막 전까지 반복을 진행한다.
+        for(int i = 0; i < data.length-1; i++) {
+            int index = i;
+            // 배열의 index인 i의 위치에 들어갈 최솟값 index를 찾는다.
+            for(int j = i + 1; j < data.length; j++) {
+                // 만약 최솟값을 찾았다면
+                if(data[j] < data[index]){
+                    // 최솟값의 index를 index 변수에 넣는다.
+                    index = j;
+                }
+            }
+
+            // 최솟값과 i번째 값을 서로 교환한다.
+            swap(data, i, index);
+        }
+
+
+        return data;
+    }
+
+    /**
+     * 최솟값(changeIndex 위치의 값)과 i번째 값을 서로 교환한다.
+     * @param data
+     * @param index
+     * @param changeIndex
+     */
+    public static void swap(int[] data, int index, int changeIndex) {
+        int temp = data[index];
+        data[index] = data[changeIndex];
+        data[changeIndex] = temp;
+    }
+}
